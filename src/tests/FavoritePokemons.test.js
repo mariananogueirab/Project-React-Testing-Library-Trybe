0867b5b2e1c5a539cb863de27c8cd024cabd2316
mariananogueirab <mariananbp@gmail.com>
2021-08-22 18:45:11 -0300

    Tentativa do Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,31 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from '../util/renderWithRouter';
+import App from '../App';
+
+describe('Test Favorite Pokemons Page', () => {
+  it('Test No Favorite Pokemon found', () => {
+    render(<FavoritePokemons />);
+    const notFountText = screen.getByText(/No favorite pokemon found/i);
+    expect(notFountText).toBeInTheDocument();
+  });
+
+  it('Test Favorite Pokemons', async () => {
+    renderWithRouter(<App />);
+    const linkMoreDetails = screen.getByRole('link', { name: 'More details' });
+    expect(linkMoreDetails).toBeInTheDocument();
+
+    userEvent.click(linkMoreDetails);
+    const testidPokemon = screen.getByTestId('pokemon-name');
+
+    const pokemonFavoritado = screen.getByRole('checkbox');
+    userEvent.click(pokemonFavoritado);
+
+    const linkFavoritePokemon = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    userEvent.click(linkFavoritePokemon);
+
+    expect(testidPokemon.value).toBeInTheDocument();
+  });
+});
