2c0e3976ef3b79d0680176a906a0ef978086bb4e
mariananogueirab <mariananbp@gmail.com>
2021-08-23 13:13:55 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import NotFound from '../components/NotFound';
+import renderWithRouter from '../util/renderWithRouter';
+
+describe('Test Not Found component', () => {
+  it('Test heading h2', () => {
+    render(<NotFound />);
+    const notFoundTitle = screen.getByRole('heading', {
+      level: 2,
+      name: /Page requested not found/i,
+    });
+    expect(notFoundTitle).toBeInTheDocument();
+  });
+
+  it('Test image', () => {
+    render(<NotFound />);
+    const imageNotFound = screen
+      .getByAltText('Pikachu crying because the page requested was not found');
+    expect(imageNotFound).toBeInTheDocument();
+    expect(imageNotFound).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
